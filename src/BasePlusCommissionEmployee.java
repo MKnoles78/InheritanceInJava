0a81a94330f4e59bb4ce5dd71096310b8e4dc44d
Mike Knoles <MKnoles78@gmail.com>
2021-12-08 01:22:26 -0500

    added new class to test

--- src/BasePlusCommissionEmployee.java
+++ src/BasePlusCommissionEmployee.java
@@ -1,4 +1,11 @@
-//BasePlusCommissionEmployee
+//BasePlusCommissionEmployee class represents an employee who receives a base salary in addition to commission
+
 public class BasePlusCommissionEmployee {
+	private final String firstName;
+	private final String lastName;
+	private final String socialSecurityNumber;
+	private double grossSales; // gross weekly sales
+	private double commissionRate; // commission percentage
+	private double baseSalary; // base salary per week
 
 }
