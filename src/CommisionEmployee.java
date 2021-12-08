9a0897d208b13c61d945a4e7682fff1c987ffb97
Mike Knoles <MKnoles78@gmail.com>
2021-12-07 23:20:31 -0500

    start constructor

--- src/CommisionEmployee.java
+++ src/CommisionEmployee.java
@@ -8,4 +8,12 @@ public class CommisionEmployee extends Object {
 	private double grossSales; // gross weekly sales
 	private double commissionRate; // commission percentage
 
+	// five-argument constructor
+	public CommissionEmployee(String firstName, String lastName, String socialSecurityNumber, double grossSales, double commissionRate) {
+		//implicit call to Object's default constructor occurs here
+		
+		
+		
+	}
+
 }
