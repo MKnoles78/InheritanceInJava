babb039d976601c11950e6db0b48101e70c96475
Mike Knoles <MKnoles78@gmail.com>
2021-12-07 23:58:54 -0500

    added top informational line

--- src/CommissionEmployeeTest.java
+++ src/CommissionEmployeeTest.java
@@ -6,6 +6,7 @@ public class CommissionEmployeeTest {
 		CommissionEmployee employee = new CommissionEmployee("Sue", "Jones", "222-22-2222", 10000, .06);
 
 		// get commission employee data
+		System.out.println("Employee information obtained by get methods: ");
 		System.out.printf("%n%s %s%n", "First name is ", employee.getFirstName());
 		System.out.printf("%s %s%n", "Last name is ", employee.getLastName());
 		System.out.printf("%s %s%n", "Social Security number is ", employee.getSociaSecurityNumber());
