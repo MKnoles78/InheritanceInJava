2579ac5d26ea95a5495690470083b5cf3cb9147b
Mike Knoles <MKnoles78@gmail.com>
2021-12-07 23:17:41 -0500

    establish instance variables

--- src/CommisionEmployee.java
+++ src/CommisionEmployee.java
@@ -1,4 +1,11 @@
+//CommissionEmployee class represents an employee paid a percentage of gross sales
 
-public class CommisionEmployee {
+public class CommisionEmployee extends Object {
+
+	private final String firstName;
+	private final String lastName;
+	private final String socialSecurityNumber;
+	private double grossSales; // gross weekly sales
+	private double commissionRate; // commission percentage
 
 }
