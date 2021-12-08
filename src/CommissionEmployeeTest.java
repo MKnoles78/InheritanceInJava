148396025de21a86155d5d06ba2bf46ef0a8d6f6
Mike Knoles <MKnoles78@gmail.com>
2021-12-07 23:57:25 -0500

    added system.out

--- src/CommissionEmployeeTest.java
+++ src/CommissionEmployeeTest.java
@@ -5,6 +5,17 @@ public class CommissionEmployeeTest {
 		// instantiate CommissionEmployee object
 		CommissionEmployee employee = new CommissionEmployee("Sue", "Jones", "222-22-2222", 10000, .06);
 
+		// get commission employee data
+		System.out.printf("%n%s %s%n", "First name is ", employee.getFirstName());
+		System.out.printf("%s %s%n", "Last name is ", employee.getLastName());
+		System.out.printf("%s %s%n", "Social Security number is ", employee.getSociaSecurityNumber());
+		System.out.printf("%s %.2f%n", "Gross sales is ", employee.getGrossSales());
+		System.out.printf("%s %.2f%n", "Commission rate is ", employee.getCommissionRate());
+
+		employee.setGrossSales(5000);
+		employee.setCommissionRate(.1);
+
+		System.out.printf("%n%s:%n%n%s%n", "Updated employee information obtained by toString", employee);
 	}
 
 }
