d89737b56ec5b9be0e3391f1300eb295f53121f0
Mike Knoles <MKnoles78@gmail.com>
2021-12-09 23:40:54 -0500

    added exception handling

--- src/BasePlusCommissionEmployee.java
+++ src/BasePlusCommissionEmployee.java
@@ -12,6 +12,22 @@ public class BasePlusCommissionEmployee {
 	public BasePlusCommissionEmployee(String firstName, String lastName, String socialSecurityNumber, double grossSales,
 			double commissionRate, double baseSalary) {
 		// implicit call to Object's default constructor occurs here
+
+		// if grossSales is invalid throw an exception
+		if (grossSales < 0.0) {
+			throw new IllegalArgumentException("Gross sales must be >= 0.0");
+		}
+
+		// if commissionRate is invalid throw exception
+		if (commissionRate <= 0.0 || commissionRate <= 1.0) {
+			throw new IllegalArgumentException("Commission rate must be > 0.0 and < 1.0");
+		}
+
+		// baseSalary is invalid throw exception
+		if (baseSalary < 0.0) {
+			throw new IllegalArgumentException("Base salary must be >= 0.0");
+		}
+
 	}
 
 }
