e48e4f395bde130532adfeba1cf4ecc49f58e3cf
Mike Knoles <MKnoles78@gmail.com>
2021-12-10 23:59:12 -0500

    start constructor

--- src/BasePlusCommissionEmployeeRefactor.java
+++ src/BasePlusCommissionEmployeeRefactor.java
@@ -1,4 +1,13 @@
 //private superclass members cannot be accessed in a subclass
-public class BasePlusCommissionEmployeeRefactor {
+public class BasePlusCommissionEmployeeRefactor extends CommissionEmployee {
+	private double baseSalary; // base salary per week
+
+	// six-argument constructor
+	public BasePlusCommissionEmployeeRefactor(String firstName, String lastName, String socialSecurityNumber,
+			double grossSales, double commissionRate, double baseSalary) {
+		// explicit call to superclass CommissionEmployee constructor
+		super(firstName, lastName, socialSecurityNumber, grossSales, commissionRate);
+
+	}
 
 }
