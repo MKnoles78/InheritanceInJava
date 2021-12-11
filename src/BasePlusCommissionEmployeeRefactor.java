84c1b0d710a02054fa636efc4b213f94e39c6b49
Mike Knoles <MKnoles78@gmail.com>
2021-12-11 00:07:50 -0500

    added methods for earnings

--- src/BasePlusCommissionEmployeeRefactor.java
+++ src/BasePlusCommissionEmployeeRefactor.java
@@ -8,6 +8,43 @@ public class BasePlusCommissionEmployeeRefactor extends CommissionEmployee {
 		// explicit call to superclass CommissionEmployee constructor
 		super(firstName, lastName, socialSecurityNumber, grossSales, commissionRate);
 
+		// if baseSalary is invalid throw exception
+		if (baseSalary < 0.0) {
+			throw new IllegalArgumentException("Base salary must be >= 0.0");
+		}
+
+		this.baseSalary = baseSalary;
+
+	}
+
+	// set base salary
+	public void setBaseSalary(double baseSalary) {
+		if (baseSalary < 0.0) {
+			throw new IllegalArgumentException("Base salary must be >= 0.0");
+		}
+		this.baseSalary = baseSalary;
+	}
+
+	// return base salary
+	public double getBaseSalary() {
+		return baseSalary;
+	}
+
+	// calculate earnings
+	@Override
+	public double earnings() {
+		// not allowed: commissionRate and grossSales private in superclass
+		return baseSalary + (commissionRate * grossSales);
+	}
+
+	// return String representation of BasePlusCommissionEmployeeRefactor
+	@Override
+	public String toString() {
+		// now allowed: attempts to access private superclass members
+		return String.format("%s: %s %s%n%s: %s%n%s: %.2f%n%s: %.2f%n%s: %.2.f", "base-salaried commission employee",
+				firstName, lastName, "social security number", socialSecurityNumber, "gross sales", grossSales,
+				"commission rate", commissionRate, "base salary", baseSalary);
+
 	}
 
 }
