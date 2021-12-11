16ad8176079be2cb74c4c48bbe141b57ff868852
Mike Knoles <MKnoles78@gmail.com>
2021-12-11 01:32:52 -0500

    adjusted syntax

--- src/CommissionEmployeeTest.java
+++ src/CommissionEmployeeTest.java
@@ -10,7 +10,7 @@ public class CommissionEmployeeTest {
 		System.out.println("Employee information obtained by get methods: ");
 		System.out.printf("%n%s %s%n", "First name is ", employee.getFirstName());
 		System.out.printf("%s %s%n", "Last name is ", employee.getLastName());
-		System.out.printf("%s %s%n", "Social Security number is ", employee.getSociaSecurityNumber());
+		System.out.printf("%s %s%n", "Social Security number is ", employee.getSocialSecurityNumber());
 		System.out.printf("%s %.2f%n", "Gross sales is ", employee.getGrossSales());
 		System.out.printf("%s %.2f%n", "Commission rate is ", employee.getCommissionRate());
 
